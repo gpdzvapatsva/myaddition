ca89ff40c939e78f6cc8ddb3187d6e8e4ecff4d8
gpdzvapatsva <gpdzvapatsva@gmail.com>
2020-09-05 18:57:19 +0200

    initial commit

---
+++ Addition/main.py
@@ -0,0 +1,10 @@
+#Variable declaration
+num1= 7
+num2=8
+answer=0
+#printinting the answer and calculating at the same time
+print(num1+num2)
+#++++++++++++++++++++++++++++++++++
+answer = num1 *num2
+
+print("Multiplication ", answer)
